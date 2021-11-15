abdf13dd8730c8eb2be852b3c32a624b988922d6
Yerson Lasso <yersonlasso05@gmail.com>
2021-11-15 17:53:00 -0500

    unstable project compile function

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,83 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { FileCompiler, ProjectCompiler, TestData } from 'dynamic-gists-client';
+import path = require('path');
+import fsLibrary = require('fs');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dynacmic-gists-js" is now active!');
+const commandNames = {
+	buildGistFile: "buildGistFile",
+	buildGistProject: "buildGistProject"
+};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dynacmic-gists-js.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dynacmic-gists-js!');
-	});
+const extensionName = "dynacmic-gists-js";
 
-	context.subscriptions.push(disposable);
+function saveCompiledFile(compiledFile:any){
+	fsLibrary.writeFile(
+		path.join(compiledFile.filePath),
+		compiledFile.content,
+		()=>{
+			console.log("created file", compiledFile.filePath);
+			vscode.window.showInformationMessage('Created new file '+ compiledFile.filePath);
+		}
+	);
 }
 
-// this method is called when your extension is deactivated
+async function buildGistFile(newFilePath?:string){
+	const activeEditor = vscode.window.activeTextEditor;
+	if(activeEditor){
+		let newCompiledFilePath = newFilePath ? newFilePath: activeEditor.document.fileName;
+		
+		const fileCompiler = new FileCompiler({
+			block: JSON.parse(activeEditor.document.getText()),
+			args: {},
+			baseRoute: newCompiledFilePath,
+			readJsonFromFile: readJsonFromFileVSC,
+		});
+		const compiledFile = await fileCompiler.compile();
+		saveCompiledFile(compiledFile);
+	}
+}
+async function readJsonFromFileVSC(path:string){
+	let document = await vscode.workspace.openTextDocument(vscode.Uri.file(path));
+	let parsedJson = JSON.parse(document.getText());
+	return parsedJson;
+}
+
+async function buildGistProject(){
+	const activeEditor = vscode.window.activeTextEditor;
+	if(activeEditor){
+		try{
+			const projectCompiler = new ProjectCompiler({
+				project: JSON.parse(activeEditor.document.getText()),
+				projectPath: path.dirname(activeEditor.document.fileName),
+				readJsonFromFile: readJsonFromFileVSC
+			});
+			const compiledFiles = await projectCompiler.compile();
+			for(const compiledFile of compiledFiles){
+				saveCompiledFile(compiledFile);
+			}
+		} catch(err){
+			console.error("caught err",err);
+		}
+		
+		
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	
+	context.subscriptions.push(vscode.commands.registerCommand(
+		`${extensionName}.${commandNames.buildGistFile}`,
+		buildGistFile
+	));
+
+	context.subscriptions.push(vscode.commands.registerCommand(
+		`${extensionName}.${commandNames.buildGistProject}`,
+		buildGistProject
+	));
+}
+
 export function deactivate() {}
