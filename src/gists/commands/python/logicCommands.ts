ced0ccb5fe80a3bcc1ee83d9855e402fd4460b92
Yerson Lasso <yersonlasso05@gmail.com>
2021-12-19 14:44:52 -0500

    functional command for adding extra elif statement

--- src/gists/commands/python/logicCommands.ts
+++ src/gists/commands/python/logicCommands.ts
@@ -1,4 +1,4 @@
-import { window, TextEditorEdit, Position} from 'vscode';
+import { window, TextEditorEdit, Position, TextEditor} from 'vscode';
 
 function getTextTabulation(text:string): number{
     let result = 0;
@@ -12,6 +12,52 @@ function getTextTabulation(text:string): number{
     return result;
 }
 
+function getElseIfBlock(startLine:number, activeEditor: TextEditor): any{
+    const initialLineCount = activeEditor.document.lineCount;
+    const editorTabSize = 4;
+    const initialLineContent = activeEditor.document.lineAt(startLine).text;
+    const initialSpacing = getTextTabulation(initialLineContent);
+    let blockEndLine = -1;
+    let subBlocks:string[][] = [[]];
+    for(let i = startLine+1; i < initialLineCount; i++){
+        const currentLineContent = activeEditor.document.lineAt(i).text;
+        const currentInitialSpacing = getTextTabulation(currentLineContent);
+        console.log('-----------------------------------------');
+        console.log('currentLineContent', currentLineContent);
+        console.log('currentInitialSpacing', currentInitialSpacing);
+        console.log('initialSpacing', initialSpacing);
+        console.log('subBlocks', subBlocks);
+        if(initialSpacing === currentInitialSpacing){
+            if(/else\s?:*$/.test(currentLineContent) || /elif.*:.*$/.test(currentLineContent)){
+                console.log('is else if or else');
+                subBlocks.push([currentLineContent]);
+                continue;
+            }
+            if(/[^\s]+$/.test(currentLineContent)){
+                console.log('is an other block');
+                blockEndLine = i;
+                break;
+            }
+        }
+        if(initialSpacing > currentInitialSpacing){
+            console.log('is end of block');
+            blockEndLine = i;
+            break;
+        }
+        subBlocks[subBlocks.length-1].push(currentLineContent);
+    } 
+    if(blockEndLine === -1){
+        blockEndLine = initialLineCount+1;
+    }
+    console.log('>---------------------------------------<');
+    return {
+        subBlocks,
+        blockEndLine,
+        editorTabSize,
+        initialSpacing,
+    };
+}
+
 export async function pythonAddElseIfCommand() {
     const activeEditor = window.activeTextEditor;
     if (!activeEditor) {
@@ -23,25 +69,13 @@ export async function pythonAddElseIfCommand() {
         // TODO: let user know this is not a valid line
         return;
     }
-    let initialSpacing = -1;
-    const initialLineCount = activeEditor.document.lineCount;
-    const editorTabSize = 4;
-    for(let i = activeLine+1; i < initialLineCount; i++){
-        const currentLineContent = activeEditor.document.lineAt(i).text;
-        const currentinitialSpacing = getTextTabulation(currentLineContent);
-        if(initialSpacing === -1){
-            initialSpacing = currentinitialSpacing;
-        }
-        if(initialSpacing !== currentinitialSpacing && initialSpacing > currentinitialSpacing){
-            await activeEditor.edit((editBuilder: TextEditorEdit) => {
-                // TODO: there are different spacing types, make sure to make this code dynamic for any valid combination
-                editBuilder.insert(new Position(i, 0), `\n${' '.repeat(initialSpacing-editorTabSize)}elif True:\n${' '.repeat(i+1)}pass\n`);
-            });
-            return;
-        }
-    }
+    const ifBlock = getElseIfBlock(activeLine, activeEditor);
+    console.log('ifBlock', ifBlock);
     await activeEditor.edit((editBuilder: TextEditorEdit) => {
-        editBuilder.insert(new Position(initialLineCount+1, 0), `\n${' '.repeat(initialSpacing-editorTabSize)}elif True:\n${' '.repeat(initialSpacing)}pass\n`);
+        editBuilder.insert(
+            new Position(ifBlock.blockEndLine-1, ifBlock.subBlocks.slice(-1)[0].slice(-1)[0].length),
+            `\n${' '.repeat(ifBlock.initialSpacing)}elif True:\n${' '.repeat(ifBlock.initialSpacing+ifBlock.editorTabSize)}pass`
+        );
     });
     return;
 }
